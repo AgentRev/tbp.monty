a13cf4b1c6a7687ca7f37b3ba97cd5965a4b4405
AgentRev <1380241+AgentRev@users.noreply.github.com>
2026-09-03 14:20:29 -0400

    refactor: telemetry_test

--- tests/unit/telemetry/telemetry_test.py
+++ tests/unit/telemetry/telemetry_test.py
@@ -16,6 +16,7 @@ import hydra
 import pytest
 
 from tbp.monty import telemetry
+from tbp.monty.frameworks.experiments.monty_experiment import MontyExperiment
 from tbp.monty.frameworks.models import monty_base
 from tbp.monty.hydra import instantiate_experiment
 from tbp.monty.telemetry.publishers import TelemetryPublisher
@@ -40,15 +41,15 @@ class TelemetryLogHandler(logging.Handler):
 
 
 class TelemetryEventTest(unittest.TestCase):
-    """Unit tests for telemetry events."""
+    """Unit tests for `TelemetryEvent`."""
 
     def test_telemetry_event_defaults(self):
-        """Verify TelemetryEvent kind fallback and values mapping."""
+        """Verify `TelemetryEvent` kind fallback and values mapping."""
         event = TelemetryEvent(kind="")
         self.assertEqual(event.kind, event.__class__.__name__)
 
     def test_telemetry_event_custom_fields(self):
-        """Verify TelemetryEvent kind when explicitly specified."""
+        """Verify `TelemetryEvent` kind when explicitly specified."""
         kind = "NewGraphAdded"
         graph_id = "new_object0"
         event = TelemetryEvent(kind=kind, graph_id=graph_id)
@@ -57,62 +58,81 @@ class TelemetryEventTest(unittest.TestCase):
 
 
 class TelemetryPublisherTest(unittest.TestCase):
-    """Unit tests for TelemetryPublisher."""
+    """Unit tests for `TelemetryPublisher`."""
 
     def setUp(self):
+        telemetry.getTelemeter("tbp.monty").setLevel(logging.NOTSET)
         self.handler = TelemetryLogHandler()
         self.telemeter = telemetry.getTelemeter(monty_base.__name__)
+        self.telemeter.setLevel(logging.NOTSET)
         self.telemeter.addHandler(self.handler)
-
-        with hydra.initialize_config_dir(version_base=None, config_dir=str(HYDRA_ROOT)):
-            self.base_cfg = hydra.compose(
-                config_name="experiment",
-                overrides=[
-                    "experiment=test/profile/base",
-                    "+telemetry=info",
-                ],
-            )
 
     def tearDown(self):
         self.telemeter.removeHandler(self.handler)
 
-    def test_config(self):
-        """Verify all telemeter configuration."""
-        with instantiate_experiment(self.base_cfg.experiment):
-            self.assertIsInstance(self.telemeter, TelemetryPublisher)
-            self.assertEqual(self.telemeter.name, f"telemetry.{monty_base.__name__}")
+    def test_params(self):
+        """Verify logger params."""
+        self.assertIsInstance(self.telemeter, TelemetryPublisher)
+        self.assertEqual(self.telemeter.name, f"telemetry.{monty_base.__name__}")
+        self.assertFalse(self.telemeter.propagate)
+        self.assertTrue(self.telemeter.hasHandlers())
+
+    def test_events(self):
+        """Verify emitting events produces log records with telemetry schemas."""
+        self.telemeter.setLevel(logging.DEBUG)
+
+        events = [
+            (
+                self.telemeter.debug,
+                logging.DEBUG,
+                TelemetryEvent(kind="DebugEvent"),
+            ),
+            (
+                self.telemeter.info,
+                logging.INFO,
+                TelemetryEvent(kind="InfoEvent"),
+            ),
+        ]
+
+        for log_func, _, event in events:
+            log_func(event)
+
+        self.assertEqual(len(self.handler.records), len(events))
+
+        for record, (_, level, event) in zip(self.handler.records, events):
+            self.assertEqual(record.levelno, level)
+            self.assertEqual(record.msg, event.kind)
+            self.assertIs(record.__dict__.get("telemetry_schema"), event)
+
+    @staticmethod
+    def _instantiate_experiment(telemetry_profile: str) -> MontyExperiment:
+        with hydra.initialize_config_dir(version_base=None, config_dir=str(HYDRA_ROOT)):
+            base_cfg = hydra.compose(
+                config_name="experiment",
+                overrides=[
+                    "experiment=test/profile/base",
+                    f"+telemetry={telemetry_profile}",
+                ],
+            )
+        return instantiate_experiment(base_cfg.experiment)
+
+    def test_debug_config(self):
+        """Verify behavior of Hydra config ``telemetry=debug``."""
+        with self._instantiate_experiment("debug"):
+            self.telemeter.debug(TelemetryEvent(kind="TestEvent"))
+            self.assertEqual(self.telemeter.getEffectiveLevel(), logging.DEBUG)
+            self.assertEqual(len(self.handler.records), 1)
+
+    def test_info_config(self):
+        """Verify behavior of Hydra config ``telemetry=info``."""
+        with self._instantiate_experiment("info"):
+            self.telemeter.debug(TelemetryEvent(kind="TestEvent"))
+            self.telemeter.info(TelemetryEvent(kind="TestEvent"))
             self.assertEqual(self.telemeter.getEffectiveLevel(), logging.INFO)
-            self.assertFalse(self.telemeter.propagate)
-            self.assertTrue(self.telemeter.hasHandlers())
+            self.assertEqual(len(self.handler.records), 1)
 
-    def test_emit_events(self):
-        """Verify emitting events emits LogRecord with attached telemetry schema."""
-        with instantiate_experiment(self.base_cfg.experiment):
-            events = [
-                (
-                    self.telemeter.info,
-                    logging.INFO,
-                    TelemetryEvent(kind="InfoEvent"),
-                ),
-                (
-                    self.telemeter.critical,
-                    logging.CRITICAL,
-                    TelemetryEvent(kind="CriticalEvent"),
-                ),
-            ]
-
-            for log_func, _, event in events:
-                log_func(event)
-
-            self.assertEqual(len(self.handler.records), len(events))
-
-            for record, (_, level, event) in zip(self.handler.records, events):
-                self.assertEqual(record.levelno, level)
-                self.assertEqual(record.msg, event.kind)
-                self.assertIs(record.__dict__.get("telemetry_schema"), event)
-
-    def test_ignored_level(self):
-        """Verify debug event is ignored with telemetry level ``INFO``."""
-        with instantiate_experiment(self.base_cfg.experiment):
-            self.telemeter.debug(TelemetryEvent(kind="DebugEvent"))
+    def test_none_config(self):
+        """Verify behavior of Hydra config ``telemetry=none``."""
+        with self._instantiate_experiment("none"):
+            self.telemeter.info(TelemetryEvent(kind="TestEvent"))
             self.assertEqual(len(self.handler.records), 0)
